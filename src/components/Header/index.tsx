51247de730599c6a55f3ad8774734c14fc9fdce2
HEINRICK7 <heinrickcostta@gmail.com>
2022-06-09 07:59:34 -0300

    create component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container, Text } from './styled'
+import { FaBitcoin } from 'react-icons/fa'
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <FaBitcoin style={{fontSize: '4rem', color: 'E3AE14'}}/>
+      <Text>Bitcoin Price</Text>
+    </Container>
+  );
+}
+
+export default Header;
