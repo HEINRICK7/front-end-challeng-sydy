cb2fe807f20dd08f42b544970ddf8201f0c7d5dc
HEINRICK7 <heinrickcostta@gmail.com>
2022-06-09 07:58:18 -0300

    create styled Cards

---
+++ src/components/Cards/styled.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
+export const Cards = styled.div`
+  width: 100%;
+  height: 100%;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+`
+export const Prices = styled.div`
+  width: 80%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 10px;
+  border: 1px solid #DCDCDC;
+  border-radius: 16px;
+
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 29px;
+  
+  color: #000000;
+`
+export const Span = styled.div`
+  color: #DCDCDC;
+  font-size: 4rem;
+  font-weight: 300;
+`
