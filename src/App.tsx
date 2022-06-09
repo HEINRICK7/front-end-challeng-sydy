5c652f220f5a38bd1d33afd25f589e9dfab6a6f4
HEINRICK7 <heinrickcostta@gmail.com>
2022-06-09 07:55:18 -0300

    modifield App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import Header from './components/Header'
+import * as C from './Styled.App'
 
-const App = () => {
+import Cards from './components/Cards'
+import Chart from './components/Chart'
+
+import { GlobalStyled } from './Globalstyled'
+
+import { FaBitcoin } from 'react-icons/fa'
+
+function App() {
   return (
-    <h1>Hello Home</h1>
+    <>
+      <GlobalStyled />
+      <C.Container>
+        <Header />
+        <C.Section>
+          <C.ContainerSection>
+          <FaBitcoin style={{fontSize: '4rem', color: 'E3AE14'}}/>
+            <C.Text>Bitcoin</C.Text>
+            <C.P>BTC</C.P>
+          </C.ContainerSection>
+        </C.Section>
+        <C.Main>
+          <C.SectionLeft>
+            <Cards />
+          </C.SectionLeft>
+          <C.SectionRight>
+            <Chart />
+          </C.SectionRight>
+        </C.Main>
+      </C.Container>
+    </>
   );
 }
 
