885e3962e4bbb72d61153ccc0145f544bf72c3c1
HEINRICK7 <heinrickcostta@gmail.com>
2022-06-11 18:21:07 -0300

    modifield component Chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,187 +1,54 @@
-import React from 'react';
-import { Line } from '@ant-design/charts';
-import { Area } from '@ant-design/plots';
+import React, { useContext } from 'react';
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import { GlobalStateContext } from '../../GlobalContext/GlobalStateContext'
+import 'antd/dist/antd.css'
+import { Container } from './styled'
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
-import { Container } from './styled';
+const Charts: React.FC = () => {
 
-const Chart: React.FC = () => {
-  const data = [
+  const { coinHistorical, loading } = useContext(GlobalStateContext)
+
+  const antIcon = <LoadingOutlined style={{ fontSize: 50 }} spin />
+  const key = coinHistorical.map((res) => (Object.keys(res.bpi)))
+  const value = coinHistorical.map((res) => (Object.values(res.bpi)))
+
+  const options: ApexOptions = {
+    xaxis: {
+      categories: key[0]
+    }
+  }
+  const series: any = [
     {
-      "Date": "2010-01",
-      "scales": 1998
-    },
-    {
-      "Date": "2010-02",
-      "scales": 1850
-    },
-    {
-      "Date": "2010-03",
-      "scales": 1720
-    },
-    {
-      "Date": "2010-04",
-      "scales": 1818
-    },
-    {
-      "Date": "2010-05",
-      "scales": 1920
-    },
-    {
-      "Date": "2010-06",
-      "scales": 1802
-    },
-    {
-      "Date": "2010-07",
-      "scales": 1945
-    },
-    {
-      "Date": "2010-08",
-      "scales": 1856
-    },
-    {
-      "Date": "2010-09",
-      "scales": 2107
-    },
-    {
-      "Date": "2010-10",
-      "scales": 2140
-    },
-    {
-      "Date": "2010-11",
-      "scales": 2311
-    },
-    {
-      "Date": "2010-12",
-      "scales": 1972
-    },
-    {
-      "Date": "2011-01",
-      "scales": 1760
-    },
-    {
-      "Date": "2011-02",
-      "scales": 1824
-    },
-    {
-      "Date": "2011-03",
-      "scales": 1801
-    },
-    {
-      "Date": "2011-04",
-      "scales": 2001
-    },
-    {
-      "Date": "2011-05",
-      "scales": 1640
-    },
-    {
-      "Date": "2011-06",
-      "scales": 1502
-    },
-    {
-      "Date": "2011-07",
-      "scales": 1621
-    },
-    {
-      "Date": "2011-08",
-      "scales": 1480
-    },
-    {
-      "Date": "2011-09",
-      "scales": 1549
-    },
-    {
-      "Date": "2011-10",
-      "scales": 1390
-    },
-    {
-      "Date": "2011-11",
-      "scales": 1325
-    },
-    {
-      "Date": "2011-12",
-      "scales": 1250
-    },
-    {
-      "Date": "2012-01",
-      "scales": 1394
-    },
-    {
-      "Date": "2012-02",
-      "scales": 1406
-    },
-    {
-      "Date": "2012-03",
-      "scales": 1578
-    },
-    {
-      "Date": "2012-04",
-      "scales": 1465
-    },
-    {
-      "Date": "2012-05",
-      "scales": 1689
-    },
-    {
-      "Date": "2012-06",
-      "scales": 1755
-    },
-    {
-      "Date": "2012-07",
-      "scales": 1495
-    },
-    {
-      "Date": "2012-08",
-      "scales": 1508
-    },
-    {
-      "Date": "2012-09",
-      "scales": 1433
-    },
-    {
-      "Date": "2012-10",
-      "scales": 1344
-    },
-    {
-      "Date": "2012-11",
-      "scales": 1201
-    },
-    {
-      "Date": "2012-12",
-      "scales": 1065
-    },
-    {
-      "Date": "2013-01",
-      "scales": 1255
-    },
-    {
-      "Date": "2013-02",
-      "scales": 1429
-    },
-    {
-      "Date": "2013-03",
-      "scales": 1398
-    },
-    {
-      "Date": "2013-04",
-      "scales": 1678
+      name: "Dolar",
+      data: value[0]
     },
   ];
 
-  const config = {
-    data,
-    xField: 'Date',
-    yField: 'scales',
-    xAxis: {
-      tickCount: 5,
-    },
-  };
   return (
-  <Container>
-    <Area style={{width: '90%', height: '50vh'}} {...config} />
-  </Container>
-  
+    <Container>
+      {
+        loading ? (
+
+          <ReactApexChart
+            options={options}
+            series={series}
+            type="line"
+            height={450}
+            width={700}
+          />
+        ) : (
+          <>
+            <Spin tip="Loading..." indicator={antIcon} />
+          </>
+        )
+      }
+
+    </Container>
+
   );
 }
 
-export default Chart;
+export default Charts;
