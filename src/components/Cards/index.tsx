1baf52616d4082439fc8cc3fcfbcaa5a932fda2c
HEINRICK7 <heinrickcostta@gmail.com>
2022-06-11 18:20:35 -0300

    modifield index.tsx

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { LoadingOutlined } from '@ant-design/icons';
+import { GlobalStateContext } from '../../GlobalContext/GlobalStateContext'
+import 'antd/dist/antd.css'
+import { Spin } from 'antd';
 import * as C from './styled'
 
 import Usd from '../../assets/estados-unidos.png'
@@ -7,22 +10,39 @@ import Gbp from '../../assets/reino-unido.png'
 import Uer from '../../assets/uniao-europeia .png'
 
 const Cards: React.FC = () => {
+  const { bitcoin, loading } = useContext(GlobalStateContext)
+  const antIcon = <LoadingOutlined style={{ fontSize: 50 }} spin />
   return (
     <C.Container>
-      <C.Cards>
-      <C.Prices>
-        <img alt="usd"src={Usd} style={{width: '8%'}}/>
-        <C.Span>|</C.Span>$ 178,000
-      </C.Prices>
-      <C.Prices>
-        <img alt="gbp"src={Gbp} style={{width: '8%'}}/>
-        <C.Span>|</C.Span>$ 278,000
-      </C.Prices>
-      <C.Prices>
-        <img alt="uer"src={Uer} style={{width: '8%'}}/>
-        <C.Span>|</C.Span>$ 278,000
-      </C.Prices>
-      </C.Cards>
+      {
+        loading ? (
+
+          <C.Cards>
+            {bitcoin.map((res, key) => (
+              <>
+                <C.Prices key={key}>
+                  <img alt="usd" src={Usd} style={{ width: '8%' }} />
+                  <C.Span>|</C.Span>&#36; {res.bpi.USD.rate_float.toLocaleString('pt-BR', { maximumFractionDigits: 2 })}
+                </C.Prices>
+                <C.Prices>
+                  <img alt="gbp" src={Gbp} style={{ width: '8%' }} />
+                  <C.Span>|</C.Span>&pound; {res.bpi.GBP.rate_float.toLocaleString('pt-BR', { maximumFractionDigits: 2, })}
+                </C.Prices>
+                <C.Prices>
+                  <img alt="uer" src={Uer} style={{ width: '8%' }} />
+                  <C.Span>|</C.Span>&euro; {res.bpi.EUR.rate_float.toLocaleString('pt-BR', { maximumFractionDigits: 2, })}
+                </C.Prices>
+              </>))}
+          </C.Cards>
+        ) : (
+          <>
+            <C.Cards>
+              <Spin tip="Loading..." indicator={antIcon} />
+            </C.Cards>
+          </>
+        )
+      }
+
     </C.Container>
   );
 }
